4d83414bf3e4f543525105c9e17cc947c0134a44
thrivebim <rago.carmineandrea@gmail.com>
2024-05-01 01:28:26 +0100

    first commit

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ st.title('Green Report Tool')
 st.write('This is a simple check-list and report export for Project with BREEAM or other sustainbility ratings.')
 st.write("Please consider that the app doesn't warn about minimum requirements. Please consult BREEAM Official Documentation to verify you oblige to the minimum criterias.")
 st.divider()
-st.markdown("Disclaimer: This is not an official calculator for BREEAM and might present inaccuracies. \n The author doesn't have any right or role in BRE. The app is done based on author's knowledge, if you have any feeback or suggestion please send an email to xxxx")
 
+st.caption("Disclaimer: This is not an official calculator for BREEAM and might present inaccuracies. \n The author doesn't have any right or role in BRE. The app is done based on author's knowledge, if you have any feeback or suggestion please send an email to xxxx")
 st.caption('App made by car')
 
